4d26f05fd6f7668acaa570edca93670ededf9c2c
Kevin McDermott <bigkevmcd@gmail.com>
2021-01-22 08:14:51 +0000

    Refactor to clarify test intent.

--- pkg/scanning/scanner_test.go
+++ pkg/scanning/scanner_test.go
@@ -22,18 +22,76 @@ func TestScanEnvironments(t *testing.T) {
 }
 
 func TestScanEnvironmentsOnlyFetchesForEnvironments(t *testing.T) {
-	r, committed := makeTestRepo(t)
-	commits, err := Scan(r, "go-demo/environments", []string{"production"})
+	r := newTestRepo(t)
+	r.addFile(t, "go-demo/environments/production/testfile.txt")
+	r.addFile(t, "go-demo/environments/dev/testfile.txt")
+	initial := r.commit(t, "Initial Commit")
+	commits, err := Scan(r.Repository, "go-demo/environments", []string{"production"})
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	want := map[string]string{
-		"production": committed["production"],
+		"production": initial,
 	}
 	if diff := cmp.Diff(want, commits); diff != "" {
 		t.Fatalf("failed to scan commits:\n%s", diff)
 	}
+}
+
+func TestScanEnvironmentsWithUnknownEnvironment(t *testing.T) {
+	r := newTestRepo(t)
+	r.addFile(t, "go-demo/environments/production/testfile.txt")
+	r.addFile(t, "go-demo/environments/dev/testfile.txt")
+	r.commit(t, "Initial Commit")
+	commits, err := Scan(r.Repository, "go-demo/environments", []string{"staging"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := map[string]string{}
+	if diff := cmp.Diff(want, commits); diff != "" {
+		t.Fatalf("failed to scan commits:\n%s", diff)
+	}
+}
+
+func newTestRepo(t *testing.T) *testRepo {
+	t.Helper()
+	fs := memfs.New()
+	r, err := git.Init(memory.NewStorage(), fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return &testRepo{Repository: r, fs: fs}
+}
+
+type testRepo struct {
+	*git.Repository
+	fs billy.Filesystem
+}
+
+func (tr *testRepo) addFile(t *testing.T, name string) {
+	t.Helper()
+	writeTestFile(t, tr.fs, name)
+	w, err := tr.Worktree()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if _, err := w.Add(name); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (tr *testRepo) commit(t *testing.T, msg string) string {
+	w, err := tr.Worktree()
+	if err != nil {
+		t.Fatal(err)
+	}
+	c, err := w.Commit("Initial Commit", &git.CommitOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	return c.String()
 }
 
 func makeTestRepo(t *testing.T) (*git.Repository, map[string]string) {
