1d416a832083ba1111c163a006f7a0075897b1ba
Kevin McDermott <bigkevmcd@gmail.com>
2021-01-22 07:36:17 +0000

    Mock the commit.

--- pkg/scanning/scanner_test.go
+++ pkg/scanning/scanner_test.go
@@ -11,16 +11,10 @@ import (
 )
 
 func TestScanEnvironments(t *testing.T) {
-	r, _ := makeTestRepo(t)
+	r, want := makeTestRepo(t)
 	commits, err := Scan(r, "go-demo/environments", []string{"dev", "production", "staging"})
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	want := map[string]string{
-		"dev":        "b776ba84f22edc22cce3277c2b0da5341f852b83",
-		"staging":    "b776ba84f22edc22cce3277c2b0da5341f852b83",
-		"production": "042e7239324d8c11afaf188c27ad17d7c471b770",
 	}
 	if diff := cmp.Diff(want, commits); diff != "" {
 		t.Fatalf("failed to scan commits:\n%s", diff)
@@ -28,23 +22,18 @@ func TestScanEnvironments(t *testing.T) {
 }
 
 func TestScanEnvironmentsOnlyFetchesForEnvironments(t *testing.T) {
-	r, want := makeTestRepo(t)
+	r, committed := makeTestRepo(t)
 	commits, err := Scan(r, "go-demo/environments", []string{"production"})
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	want := map[string]string{
+		"production": committed["production"],
+	}
 	if diff := cmp.Diff(want, commits); diff != "" {
 		t.Fatalf("failed to scan commits:\n%s", diff)
 	}
-}
-
-func openTestRepo(t *testing.T) *git.Repository {
-	r, err := git.PlainOpen("../../../gitops-repo")
-	if err != nil {
-		t.Fatal(err)
-	}
-	return r
 }
 
 func makeTestRepo(t *testing.T) (*git.Repository, map[string]string) {
@@ -55,16 +44,20 @@ func makeTestRepo(t *testing.T) (*git.Repository, map[string]string) {
 		t.Fatal(err)
 	}
 
-	writeTestFile(t, fs, "go-demo/environments/production/testfile.txt")
 	w, err := repo.Worktree()
 	if err != nil {
 		t.Fatal(err)
 	}
+	writeTestFile(t, fs, "go-demo/environments/production/testfile.txt")
 	if _, err := w.Add("go-demo/environments/production/testfile.txt"); err != nil {
 		t.Fatal(err)
 	}
 	writeTestFile(t, fs, "go-demo/environments/dev/testfile.txt")
 	if _, err := w.Add("go-demo/environments/dev/testfile.txt"); err != nil {
+		t.Fatal(err)
+	}
+	writeTestFile(t, fs, "go-demo/environments/staging/testfile.txt")
+	if _, err := w.Add("go-demo/environments/staging/testfile.txt"); err != nil {
 		t.Fatal(err)
 	}
 
@@ -74,6 +67,8 @@ func makeTestRepo(t *testing.T) (*git.Repository, map[string]string) {
 		t.Fatal(err)
 	}
 	envs["production"] = c.String()
+	envs["staging"] = c.String()
+	envs["dev"] = c.String()
 	return repo, envs
 }
 
