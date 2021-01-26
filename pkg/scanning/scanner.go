aa4515963c113d97ee54b1eeceb16da89ab009ed
Kevin McDermott <bigkevmcd@gmail.com>
2021-01-26 08:41:34 +0000

    Add support for detecting changed environments in a commit.
    This adds a new ChangedEnvironments function that can detect the environments
    changed within a specific sha.

--- pkg/scanning/scanner.go
+++ pkg/scanning/scanner.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/storer"
 	"github.com/go-git/go-git/v5/utils/merkletrie"
@@ -88,6 +89,70 @@ func Scan(r *git.Repository, base string, environments []string) (map[string]str
 		return nil, fmt.Errorf("failed to scan commits: %w", err)
 	}
 	return envCommits, nil
+}
+
+func ChangedEnvironments(r *git.Repository, base string, h plumbing.Hash) ([]string, error) {
+	foundEnvs := map[string]bool{}
+	commit, err := r.CommitObject(h)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get the commit object from repository in %v: %w", r, err)
+	}
+
+	hash := commit.Hash.String()
+	currentDirState, err := commit.Tree()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tree for commit %s: %w", hash[:7], err)
+	}
+
+	prevCommitObject, err := commit.Parents().Next()
+	if err != nil {
+		if err != io.EOF {
+			return nil, fmt.Errorf("failed to get the next parent for commit %s: %w", hash[:7], err)
+		}
+		files := currentDirState.Files()
+		defer files.Close()
+
+		files.ForEach(func(f *object.File) error {
+			env := envName(f.Name, base)
+			foundEnvs[env] = true
+			return nil
+		})
+	}
+
+	// TODO: what does this really mean?
+	if prevCommitObject == nil {
+		return []string{}, nil
+	}
+	prevDirState, err := prevCommitObject.Tree()
+	if err != nil {
+		return nil, fmt.Errorf("could not get tree from previous commit: %w", err)
+	}
+	changes, err := prevDirState.Diff(currentDirState)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get previous dir state diff: %w", err)
+	}
+	for _, ch := range changes {
+		action, err := ch.Action()
+		if err != nil {
+			return nil, fmt.Errorf("could not get the action for change %s: %w", ch, err)
+		}
+		if action == merkletrie.Modify {
+			filename := ch.To.Name
+			if ch.From != emptyChange {
+				filename = ch.From.Name
+			}
+			env := envName(filename, base)
+			foundEnvs[env] = true
+		}
+	}
+	if err != nil && err != io.EOF {
+		return nil, fmt.Errorf("failed to scan commits: %w", err)
+	}
+	found := []string{}
+	for k := range foundEnvs {
+		found = append(found, k)
+	}
+	return found, nil
 }
 
 func removeEmpty(s []string) []string {
--- pkg/scanning/scanner_test.go
+++ pkg/scanning/scanner_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/storage/memory"
 	"github.com/google/go-cmp/cmp"
 )
@@ -55,6 +56,23 @@ func TestScanEnvironmentsWithUnknownEnvironment(t *testing.T) {
 	}
 }
 
+func TestChangedEnvironments(t *testing.T) {
+	r := newTestRepo(t)
+	r.addFile(t, "go-demo/environments/production/testfile.txt")
+	r.commit(t, "Initial Commit")
+	r.addFileWithContent(t, "go-demo/environments/production/testfile.txt", "testfile1")
+	initial := r.commit(t, "Update Files")
+	envs, err := ChangedEnvironments(r.Repository, "go-demo/environments", plumbing.NewHash(initial))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := []string{"production"}
+	if diff := cmp.Diff(want, envs); diff != "" {
+		t.Fatalf("failed to scan changed environments for %q:\n%s", initial, diff)
+	}
+}
+
 func newTestRepo(t *testing.T) *testRepo {
 	t.Helper()
 	fs := memfs.New()
@@ -72,7 +90,12 @@ type testRepo struct {
 
 func (tr *testRepo) addFile(t *testing.T, name string) {
 	t.Helper()
-	writeTestFile(t, tr.fs, name)
+	tr.addFileWithContent(t, name, name)
+}
+
+func (tr *testRepo) addFileWithContent(t *testing.T, name, body string) {
+	t.Helper()
+	writeTestFile(t, tr.fs, name, body)
 	w, err := tr.Worktree()
 	if err != nil {
 		t.Fatal(err)
@@ -106,15 +129,15 @@ func makeTestRepo(t *testing.T) (*git.Repository, map[string]string) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	writeTestFile(t, fs, "go-demo/environments/production/testfile.txt")
+	writeTestFile(t, fs, "go-demo/environments/production/testfile.txt", "go-demo/environments/production/testfile.txt")
 	if _, err := w.Add("go-demo/environments/production/testfile.txt"); err != nil {
 		t.Fatal(err)
 	}
-	writeTestFile(t, fs, "go-demo/environments/dev/testfile.txt")
+	writeTestFile(t, fs, "go-demo/environments/dev/testfile.txt", "go-demo/environments/dev/testfile.txt")
 	if _, err := w.Add("go-demo/environments/dev/testfile.txt"); err != nil {
 		t.Fatal(err)
 	}
-	writeTestFile(t, fs, "go-demo/environments/staging/testfile.txt")
+	writeTestFile(t, fs, "go-demo/environments/staging/testfile.txt", "go-demo/environments/staging/testfile.txt")
 	if _, err := w.Add("go-demo/environments/staging/testfile.txt"); err != nil {
 		t.Fatal(err)
 	}
@@ -130,14 +153,14 @@ func makeTestRepo(t *testing.T) (*git.Repository, map[string]string) {
 	return repo, envs
 }
 
-func writeTestFile(t *testing.T, fs billy.Filesystem, name string) {
+func writeTestFile(t *testing.T, fs billy.Filesystem, name, body string) {
 	t.Helper()
 	newFile, err := fs.Create(name)
 	defer newFile.Close()
 	if err != nil {
 		t.Fatal(err)
 	}
-	if _, err := newFile.Write([]byte(name)); err != nil {
+	if _, err := newFile.Write([]byte(body)); err != nil {
 		t.Fatal(err)
 	}
 }
